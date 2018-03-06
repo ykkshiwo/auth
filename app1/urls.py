e39d150407b05f41fbfb4f7be2ce8c5da35cdd52
yangkangkai <452306233@qq.com>
2018-03-06 15:19:27 +0800

    20180306_1519

---
+++ app1/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+
+
+app_name = "app1"
+urlpatterns = [
+    url(r'^regist', views.regist_, name='regist'),
+    url(r'^login', views.login_, name='login'),
+    #url(r'^a_y_l', views.are_you_login, name='are_you_login'),
+    url(r'^logout', views.logout_, name='logout'),
+]
---
+++ app1/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+
+
+def regist_(request):
+    print(" diao yong")
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        print(username, email, password)
+        user = User.objects.create_user(username=str(username), email=str(email), password=str(password))
+        user.save()
+        return HttpResponse("regist success")
+    else:
+        return render(request, 'regist.html')
+
+
+def login_(request):
+    print("diaoyong login")
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        print(username, password)
+        user = authenticate(username=str(username), password=str(password))
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponse("login success")
+        else:
+            return HttpResponse("something wrong")
+    else:
+        return render(request, 'login.html')
+
+
+def logout_(request):
+    logout(request)
+    return HttpResponse("已经退出")
+
+'''
+@login_required(login_url='')
+def are_you_login(request):
+    return render(request, 'index.html')
+    '''
